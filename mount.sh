8c52bd5d5f647714966222f991edea370f60b812
Javier Ruiz Calle <jarc0212@gmail.com>
2016-10-10 17:16:20 +0200

    Correction

--- mount.sh
+++ mount.sh
@@ -2,15 +2,12 @@
 # Script para el montaje de un directorio en una maquina destino
 
 function main {
-  echo "hello from `basename $0`"
+  echo "hello from `basename $0`" 
   # Let's try a ssh
-  # Primero tenemos que copiar todos los archivos necesarios a la maquina que nos digan
 
   echo "This is the ip I need to connect: $1"
-
-  # Conf files should be at the same level as configurar_cluster
   if [[ -z data.tar ]]
-  then
+  then 
     rm data.tar
   fi
   tar -cf data.tar mount_raid.conf configurar_cluster.sh
