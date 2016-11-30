e2c3cca54a9af74a6b16eccadecaeef03009f4d5
Javier Ruiz Calle <jarc0212@gmail.com>
2016-11-30 19:24:40 +0100

    NIS services terminated, testing and tutorship pending

---
+++ remote_conf/nis_client.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+# Formato fichero:
+#   nombre-dominio-nis
+#   servicio-nis-al-que-se-desea-conectar (IP)
+
+function check_ip {
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+  then
+    OIFS=$IFS
+    IFS="."
+    set -- $ip
+    if [[ $1 -le 255 && $2 -le 255 && $3 -le 255 && $4 -le 255 ]]
+    then
+      echo -e "[\e[32mINFO\e[0m] Valid IP :)"
+    fi
+    IFS=$OIFS
+  else
+    echo -e "[\e[31mERROR\e[0m] Not a valid IP"
+    exit 32
+  fi
+}
+
+counterLine=0
+while read -r line
+do
+  counterLine=$((counterLine+1))
+  [[ $counterLine -eq 1 ]] && domainName=$line
+  if [[ $counterLine -eq 2 ]]
+  then
+    ip=$line
+    check_ip "$ip"
+  fi
+done < "nis_client.conf"
+
+[[ $counterLine -gt 2 ]] && exit 33
+
+which rpcbind > /dev/null
+if [[ $? -ne 0 ]] 
+then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -qq install -m portmap --no-install-recommends > /dev/null
+else
+  echo -e "[\e[32mINFO\e[0m] Command is already installed, continuing"
+fi
+
+# Igual que para el server
+# update-rc.d portmap defaults 10
+
+# Aunque no sea el que vayamos a utilizar, si este no esta instalado,
+# ninguno lo está, por lo que tenemos que instalarlo
+which ypserv > /dev/null
+if [[ $? -ne 0 ]]
+then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -qq install nis --no-install-recommends > /dev/null
+else
+  echo -e "[\e[32mINFO\e[0m] Command is already installed, continuing"
+fi
+
+# Mirar comprobaciones fancies para poner aqui
+# porque a lo mejor puede que ya exista
+echo "domain $domainName server $ip" >> /etc/yp.conf
+
+
+exit 0
--- remote_conf/nis_server.sh
+++ remote_conf/nis_server.sh
@@ -6,9 +6,60 @@ do
   lineCounter=$((lineCounter + 1))
   domainName=$line
   [[ $lineCounter -gt 1 ]] && exit 28 
+  echo -e "[\e[32mINFO\e[0m] Domain name is: $domainName"
 done < "nis_server.conf"
 
 # Check for installation of the requiered packages
-which 
+which rpcbind > /dev/null
+if [[ $? -ne 0 ]] 
+then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get install portmap --no-install-recommends
+  # Condicion, ver si se ha instalado bien
+  echo -e "[\e[32mINFO\e[0m] Command 'portmap' installed correctly"
+else
+  echo -e "[\e[32mINFO\e[0m] Command is already installed, continuing"
+fi
+
+echo -e "[\e[32mINFO\e[0m] Updating 'portmap' defaults"
+# solo parece que da problemas, vamos a intentar seguir sin este comando
+#update-rc.d portmap defaults 10
+# Condicion
+
+which ypserv > /dev/null
+if [[ $? -ne 0 ]] 
+then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -qq install nis --no-install-recommends > /dev/null
+  [[ $? -ne 0 ]] && exit 29
+  echo -e "[\e[32mINFO\e[0m] Commands for 'NIS_server' installation went well, continuing"
+else
+  echo -e "[\e[32mINFO\e[0m] Necessary commands for 'NIS_server' are already installed, continuing"
+fi
+
+# Now we edit the domain name 
+nisdomainname $domainName
+[[ $? -ne 0 ]] && exit 30
+
+if [[ -n "`grep "NISSERVER=false" /etc/default/nis`" ]]
+then
+  sed -i 's/NISSERVER=false/NISSERVER=master/g' /etc/default/nis
+fi
+
+if [[ -n "`grep "NISCLIENT=true" /etc/default/nis`" ]]
+then
+  sed -i 's/NISCLIENT=true/NISCLIENT=false/g' /etc/default/nis
+fi
+
+# TODO: Pensar bien en si hay que hacer esto, por ahora se comenta
+#sed -i 's/0.0.0.0/#0.0.0.0/g' /etc/ypserv.securenets
+
+# Restart the portmap daemon
+/etc/init.d/portmap restart
+
+# Restart the NIS daemon
+/etc/init.d/nis restart
+
+# No seguimos avanzando, tecnicamente el servidor NIS ya esta creado
 
 exit 0
